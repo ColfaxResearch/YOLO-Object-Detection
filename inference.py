ce4ce090839a740f5b629ce5763c85edd2317f3d
sangameshnr <sangameshnr@gmail.com>
2017-11-09 12:28:37 -0800

    colors to inference.py

--- inference.py
+++ inference.py
@@ -2,16 +2,39 @@
 import numpy as np
 import tensorflow as tf
 from yolo_model import TINY_YOLO as yolo
-#from model_tf_nchw import TINY_YOLO as yolo
 import cv2
 from matplotlib import pyplot as plt
-from scipy.linalg import norm
 import time
 import os, sys
 
 
 
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+color={"aeroplane":(0,0,255), "bicycle":(0,255,0), "bird":(127,127,127), "boat":(127,0,127), "bottle":(127,127,0),"bus":(0,127,127), "car":(0,255,0), "cat":(0,0,255),"chair":(180,180,0), "cow":(0,180,180), "diningtable":(180,0,180), "dog":(100,0,100), "horse":(0,0,255), "motorbike":(0,100,100), "person":(0,255,255), "pottedplant":(100,100,0), "sheep":(80,80,80), "sofa":(90,150,0), "train":(0,150,100), "tvmonitor":(100,150,0) }
+
+    
+    
+    
+def draw_boxes(img2,box_preds):
+    boxes =box_preds['boxes']
+    indices = box_preds['indices']
+    class_names = box_preds['class_names']
+
+    boxes = [boxes[i] for i in indices]
+    class_names = [class_names[i] for i in indices]
+    for i,b in enumerate(boxes):
+        left = int(max(0,b[0]))
+        bot  = int(max(0,b[1]))
+        #right= int(min(311,b[2]*0.75))
+        #top  = int(min(311,b[3]*0.75))
+        right= int(min(415,b[2]))
+        top  = int(min(415,b[3]))
+        name = class_names[i]
+        clr = color[name]
+        cv2.rectangle(img2,(left,bot),(right,top),clr,2)
+        cv2.putText(img2, name, (int(left), int(bot)+16), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,0), 2);
+
+
+
 
 
 with tf.Graph().as_default():
@@ -19,57 +42,56 @@ with tf.Graph().as_default():
     
     feed={}
 
+
     
-    #config = tf.ConfigProto()
-    #config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
-    #sess_xla = tf.Session(config=config)
+    sess_par = tf.Session(config=tf.ConfigProto(inter_op_parallelism_threads=int(os.environ['NUM_INTER_THREADS']),intra_op_parallelism_threads=int(os.environ['NUM_INTRA_THREADS'])))
     
-    cam = cv2.VideoCapture(int(sys.argv[1]))     
-    with tf.Session(config=tf.ConfigProto(inter_op_parallelism_threads=int(os.environ['NUM_INTER_THREADS']),intra_op_parallelism_threads=int(os.environ['NUM_INTRA_THREADS']))) as sess:
+    
+    #cam = cv2.VideoCapture(0)     
+    
+    #cam = cv2.VideoCapture('road.avi')     
+    videofile = sys.argv[1]
+    cam=cv2.VideoCapture(videofile)
+    Nframes = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    with sess_par as sess:
         clf = yolo(sess)
-        fps=0.0
+        fps = 0
         count = 0
         time_sum = 0.0
-        cam_fps=0.0
-        cam_count = 0
-        cam_time_sum = 0.0
-
+        frame_count =0
         while(True):
-            #if count > 30:
-            #  break
-            #count+=1
-            #path='samples/dog.jpg'
-            #img=cv2.imread(path)
-            t1=time.time()
-	    ret,img = cam.read()
-            img2=cv2.resize(cv2.flip(img,1),(416,416))
-            image = (img2*0.003921569)
-            t2=time.time()
 
+            t1=time.time()
+            ret,img=cam.read()
+            frame_count+=1
+            #if frame_count==Nframes-1:
+            #    frame_count=0
+            #    #cam=cv2.VideoCapture('road.avi')
+            #    cam=cv2.VideoCapture('caltech/set00/V001.seq')
+            if ret==False:
+                cam=cv2.VideoCapture(videofile)
+                ret,img=cam.read()
+                assert ret
+                 
+
+
+            #img=cv2.flip(img,1)
+            img2=cv2.resize(img,(416,416))
+            #image = np.zeros((416,416,3),dtype='float32')
+            image = (img2*0.003921569)
+
+            t2=time.time()
             feed=clf.create_feed_dict([image])
             box_preds=sess.run(clf.preds,feed)
-            #box_preds=sess.run(clf.box_preds,feed)
 
             t3=time.time()
-
-            boxes =box_preds['boxes']
-            indices = box_preds['indices']
-            class_names = box_preds['class_names']
-            
-            boxes = [boxes[i] for i in indices]
-            class_names = [class_names[i] for i in indices]
-            for i,b in enumerate(boxes):
-                left = int(max(1,b[0]))
-                bot  = int(max(1,b[1]))
-                right= int(min(415,b[2]))
-                top  = int(min(415,b[3]))
-                cv2.rectangle(img2,(left,bot),(right,top),(0,255,255),2)
-                cv2.putText(img2, class_names[i], (int(left), int(bot)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2);
+            draw_boxes(img2,box_preds)
             img2=cv2.resize(img2,(312,312))
             cv2.putText(img2, '%.1f fps' % (fps), (2,15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2);
-	    cv2.imshow('webcam', img2)
-	    if cv2.waitKey(1) == 27:
-		break
+            cv2.imshow('frame',img2)
+            if cv2.waitKey(1) == 27:
+                break
             t4=time.time()
             time_sum+=t4-t1
             #time_sum+=t3-t2
@@ -79,15 +101,13 @@ with tf.Graph().as_default():
               time_sum=0
               count = 0  
 
-            print "--------------------------------------- "
+            print " "
             print "Image load time : "+str(t2-t1)+" seconds"
             print "YOLO time       : "+str(t3-t2)+" seconds"
             print "Image out time  : "+str(t4-t3)+" seconds"
             print "----------------------------------------"
             print "Total time      : "+str(t4-t1)+" seconds"
-            print "----------------------------------------"
+            print " "
 
-	
-            #plt.imshow(img2)
-            #plt.show()
+
 
