3259d9bd90af893c77f96060225bbb3c3b62996e
rasai <ryoasai408@gmail.com>
2017-11-20 14:44:10 -0800

    Added V@_VOC

--- TensorFlow/inference.py
+++ TensorFlow/inference.py
@@ -1,15 +1,17 @@
 #!/opt/intel/intelpython2/bin/python
 import numpy as np
 import tensorflow as tf
-from yolo_model import TINY_YOLO as yolo
+from yolo_model import YOLO
 import cv2
 from matplotlib import pyplot as plt
 import time
 import os, sys
+from scipy.misc import imread,imsave,imresize
 
 color={"aeroplane":(0,0,255), "bicycle":(0,255,0), "bird":(127,127,127), "boat":(127,0,127), "bottle":(127,127,0),"bus":(0,127,127), "car":(0,255,0), "cat":(0,0,255),"chair":(180,180,0), "cow":(0,180,180), "diningtable":(180,0,180), "dog":(100,0,100), "horse":(0,0,255), "motorbike":(0,100,100), "person":(0,255,255), "pottedplant":(100,100,0), "sheep":(80,80,80), "sofa":(90,150,0), "train":(0,150,100), "tvmonitor":(100,150,0) }
-    
-def draw_boxes(img2,box_preds):
+def draw_boxes(img,box_preds):
+
+    batch_addr = box_preds['batch_addr']
     boxes =box_preds['boxes']
     indices = box_preds['indices']
     class_names = box_preds['class_names']
@@ -17,89 +19,35 @@ def draw_boxes(img2,box_preds):
     boxes = [boxes[i] for i in indices]
     class_names = [class_names[i] for i in indices]
     for i,b in enumerate(boxes):
+        idx  = batch_addr[i]
         left = int(max(0,b[0]))
         bot  = int(max(0,b[1]))
         right= int(min(415,b[2]))
         top  = int(min(415,b[3]))
-        name = class_names[i]
-        clr = color[name]
-        cv2.rectangle(img2,(left,bot),(right,top),clr,2)
-        cv2.putText(img2, name, (int(left), int(bot)+16), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,0), 2);
-
-
-
+        cv2.rectangle(img[idx],(left,bot),(right,top),(0,255,255),2)
+        cv2.putText(img[idx], class_names[i], (int(left), int(bot)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2);
 
 
 with tf.Graph().as_default():
-    
-    
-    feed={}
+  feed={}
+  sess_par = tf.Session(config=tf.ConfigProto(inter_op_parallelism_threads=int(os.environ['NUM_INTER_THREADS']),intra_op_parallelism_threads=int(os.environ['NUM_INTRA_THREADS'])))
+  
+  img_in = tf.placeholder(tf.float32,[None,416,416,3])
+  clf = YOLO(img_in, 'V2_VOC')
+  #clf = YOLO(img_in)
+  saver = tf.train.Saver()
+  img=cv2.imread(sys.argv[1])
+  img2=cv2.resize(img,(416,416))
+  image = [(img2*0.003921569)]*10
 
-
-    
-    sess_par = tf.Session(config=tf.ConfigProto(inter_op_parallelism_threads=int(os.environ['NUM_INTER_THREADS']),intra_op_parallelism_threads=int(os.environ['NUM_INTRA_THREADS'])))
-    
-    
-    #cam = cv2.VideoCapture(0)     
-    #cam = cv2.VideoCapture('road.avi')     
-    videofile = sys.argv[1]
-    cam=cv2.VideoCapture(videofile)
-    Nframes = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    with sess_par as sess:
-        clf = yolo(sess)
-        fps = 0
-        count = 0
-        time_sum = 0.0
-        frame_count =0
-        while(True):
-
-            t1=time.time()
-            ret,img=cam.read()
-            frame_count+=1
-            #if frame_count==Nframes-1:
-            #    frame_count=0
-            #    #cam=cv2.VideoCapture('road.avi')
-            #    cam=cv2.VideoCapture('caltech/set00/V001.seq')
-            if ret==False:
-                cam=cv2.VideoCapture(videofile)
-                ret,img=cam.read()
-                assert ret
-                 
-
-
-            #img=cv2.flip(img,1)
-            img2=cv2.resize(img,(416,416))
-            #image = np.zeros((416,416,3),dtype='float32')
-            image = (img2*0.003921569)
-
-            t2=time.time()
-            feed=clf.create_feed_dict([image])
-            box_preds=sess.run(clf.preds,feed)
-
-            t3=time.time()
-            draw_boxes(img2,box_preds)
-            img2=cv2.resize(img2,(312,312))
-            cv2.putText(img2, '%.1f fps' % (fps), (2,15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2);
-            #cv2.imshow('frame',img2)
-            if cv2.waitKey(1) == 27:
-                break
-            t4=time.time()
-            time_sum+=t4-t1
-            #time_sum+=t3-t2
-            count+=1
-            if time_sum > 0.5:
-              fps=count/(time_sum)
-              time_sum=0
-              count = 0  
-
-            print " "
-            print "Image load time : "+str(t2-t1)+" seconds"
-            print "YOLO time       : "+str(t3-t2)+" seconds"
-            print "Image out time  : "+str(t4-t3)+" seconds"
-            print "----------------------------------------"
-            print "Total time      : "+str(t4-t1)+" seconds"
-            print " "
-
-
-
+  with sess_par as sess:
+    saver.restore(sess, "models/original-v2-voc-model.ckpt")
+    #saver.restore(sess, "models/original-tiny-voc-model.ckpt")
+    for i in xrange(20):
+      t0 = time.time()
+      box_preds=sess.run(clf.preds,{img_in: image})
+      t1 = time.time()
+      print("%d: %f"  % (i, t1-t0))
+    draw_boxes(img2,box_preds)
+    cv2.imwrite('output.jpg',img2)
+    print("Output image saved to output.jpg")
--- TensorFlow/yolo_model.py
+++ TensorFlow/yolo_model.py
@@ -1,187 +1,190 @@
 import tensorflow as tf 
 import numpy as np
-from abc import ABCMeta, abstractmethod
 
-CNHW =[[16,3,3,3],[32,3,3,16],[64,3,3,32],[128,3,3,64],[256,3,3,128],[512,3,3,256],[1024,3,3,512],[1024,3,3,1024],[125,1,1,1024]]
+###
+# Supported yolo-types
+#  TINY_VOC
+#  V2_VOC
+###
+class YOLO:
+  def __init__(self, input_images, yolotype='TINY_VOC'):
+    #tf.input_placeholder(tf.float32,[None,416,416,3])
+    self.weights =[]
+    self.biases =[]
+    self.trainable=False
+    self.input_images = input_images
+    self.yolotype=yolotype
+    if self.yolotype == 'V2_VOC':
+      self.num_classes = 20
+      self.cl_names=np.array(["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"])
+      self.anchors = np.array([1.3221, 1.73145, 3.19275, 4.00944, 5.05587, 8.09892, 9.47112, 4.84053, 11.2364, 10.0071],dtype='float32')
+      self.conv_pipe = self.yoloV2ConvPipe(self.input_images)
+    elif self.yolotype == 'TINY_VOC':
+      self.num_classes = 20
+      self.cl_names=np.array(["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"])
+      self.anchors = np.array([1.08,1.19,3.42,4.41,6.63,11.38,9.42,5.11,16.62,10.52],dtype='float32')
+      self.conv_pipe = self.tinyConvPipe(self.input_images)
+    else:
+      print("Unkown yolotype:")
+      assert False
+    self.preds = self.NMSPipe(self.conv_pipe)
 
-anchors = np.array([1.08,1.19,3.42,4.41,6.63,11.38,9.42,5.11,16.62,10.52],dtype='float32')
-cl_name=np.array(["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"])
+  def _leakyConv(self,input_layer, shape, stride=[1,1,1,1]):
+    w = tf.Variable(tf.truncated_normal(shape,dtype=tf.float32,stddev=1e-1),name='weights',trainable=self.trainable)
+    b = tf.Variable(tf.constant(0.0,shape=[shape[3]],dtype=tf.float32),trainable=self.trainable,name='biases')
+    conv = tf.nn.bias_add(tf.nn.conv2d(input_layer,w,stride,padding='SAME'), b)
+    self.weights +=[w]
+    self.biases +=[b]
+    relu = tf.maximum(conv,tf.scalar_mul(0.1,conv))
+    return relu
 
+  def _linearConv(self,input_layer, shape, stride=[1,1,1,1]):
+    w = tf.Variable(tf.truncated_normal(shape,dtype=tf.float32,stddev=1e-1),name='weights',trainable=self.trainable)
+    b = tf.Variable(tf.constant(0.0,shape=[shape[3]],dtype=tf.float32),trainable=self.trainable,name='biases')
+    conv = tf.nn.bias_add(tf.nn.conv2d(input_layer,w,stride,padding='SAME'), b)
+    self.weights +=[w]
+    self.biases +=[b]
+    return conv
 
-#class YOLO_MODEL:
-#    def __init__(self,sess=None):
-#        self.add_placeholder()
-#        self.box_preds=self.convlayers()
-#        self.preds = self.get_boxes(self.box_preds)
-#        if sess is not None:
-#            self.load_params(sess)        
-#
-#    def leaky_activation(self,input_layer):
-#        return tf.maximum(input_layer,tf.scalar_mul(0.1,input_layer))
-#        #return tf.nn.leaky_relu(input_layer,alpha=0.1)
-
-class TINY_YOLO:
-
-    def __init__(self,sess=None):
-        self.add_placeholder()
-        self.box_preds=self.convlayers()
-        self.preds = self.get_boxes(self.box_preds)
-
-    def add_placeholder(self):
-        self.input_placeholder = tf.placeholder(tf.float32,[None,416,416,3])
-
-
-    def create_feed_dict(self,image):
-        feed_dict ={}
-        feed_dict[self.input_placeholder]=image
-
-        return feed_dict
-
-    def leaky_activation(self,input_layer):
-        return tf.maximum(input_layer,tf.scalar_mul(0.1,input_layer))
-        #return tf.nn.leaky_relu(input_layer,alpha=0.1)
-
-    def convlayers(self, trainable=False):
-        with tf.name_scope('tinyyolo') as scope:
-          self.weights=[]
-          self.biases =[]
+  def tinyConvPipe(self, input_images):
+    with tf.name_scope('tinyyolo') as scope:
+      with tf.name_scope('conv1') as scope:
+        conv1 = self._leakyConv(input_images, [3,3,3,16])
   
-          #conv1
-          with tf.name_scope('conv1') as scope:
-              kernel=tf.Variable(tf.truncated_normal([3,3,3,16],dtype=tf.float32,stddev=1e-1),name='weights',trainable=trainable)
-              biases = tf.Variable(tf.constant(0.0,shape=[16],dtype=tf.float32),trainable=trainable,name='biases')
-              conv = tf.nn.conv2d(self.input_placeholder,kernel,[1,1,1,1],padding='SAME')
-              out = tf.nn.bias_add(conv,biases)
-              self.conv1 = self.leaky_activation(out)
-              self.weights +=[kernel]
-              self.biases +=[biases]
+      pool1 = tf.nn.max_pool(conv1,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool1')
   
-          self.pool1 = tf.nn.max_pool(self.conv1,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool1')
+      with tf.name_scope('conv2') as scope:
+        conv2 = self._leakyConv(pool1, [3,3,16,32])
+      pool2 = tf.nn.max_pool(conv2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool2')
+          
+      with tf.name_scope('conv3') as scope:
+        conv3 = self._leakyConv(pool2, [3,3,32,64])
+      pool3 = tf.nn.max_pool(conv3,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool3')
   
-          with tf.name_scope('conv2') as scope:
-              kernel=tf.Variable(tf.truncated_normal([3,3,16,32],dtype=tf.float32,stddev=1e-1),name='weights',trainable=trainable)
-              biases = tf.Variable(tf.constant(0.0,shape=[32],dtype=tf.float32),trainable=trainable,name='biases')
-              conv = tf.nn.conv2d(self.pool1,kernel,[1,1,1,1],padding='SAME')
-              out = tf.nn.bias_add(conv,biases)
-              self.conv2 = self.leaky_activation(out)
-              self.weights +=[kernel]
-              self.biases +=[biases]
-          
-          self.pool2 = tf.nn.max_pool(self.conv2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool2')
-          
-          with tf.name_scope('conv3') as scope:
-              kernel=tf.Variable(tf.truncated_normal([3,3,32,64],dtype=tf.float32,stddev=1e-1),name='weights',trainable=trainable)
-              biases = tf.Variable(tf.constant(0.0,shape=[64],dtype=tf.float32),trainable=trainable,name='biases')
-              conv = tf.nn.conv2d(self.pool2,kernel,[1,1,1,1],padding='SAME')
-              out = tf.nn.bias_add(conv,biases)
-              self.conv3 = self.leaky_activation(out)
-              self.weights +=[kernel]
-              self.biases +=[biases]
-          
-          self.pool3 = tf.nn.max_pool(self.conv3,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool3')
+      with tf.name_scope('conv4') as scope:
+        conv4 = self._leakyConv(pool3, [3,3,64,128])
+      pool4 = tf.nn.max_pool(conv4,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool4')
   
-          with tf.name_scope('conv4') as scope:
-              kernel=tf.Variable(tf.truncated_normal([3,3,64,128],dtype=tf.float32,stddev=1e-1),name='weights',trainable=trainable)
-              biases = tf.Variable(tf.constant(0.0,shape=[128],dtype=tf.float32),trainable=trainable,name='biases')
-              conv = tf.nn.conv2d(self.pool3,kernel,[1,1,1,1],padding='SAME')
-              out = tf.nn.bias_add(conv,biases)
-              self.conv4 = self.leaky_activation(out)
-              self.weights +=[kernel]
-              self.biases +=[biases]
-          
-          self.pool4 = tf.nn.max_pool(self.conv4,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool4')
+      with tf.name_scope('conv5') as scope:
+        conv5 = self._leakyConv(pool4, [3,3,128,256])
+      pool5 = tf.nn.max_pool(conv5,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool5')
   
-          with tf.name_scope('conv5') as scope:
-              kernel=tf.Variable(tf.truncated_normal([3,3,128,256],dtype=tf.float32,stddev=1e-1),name='weights',trainable=trainable)
-              biases = tf.Variable(tf.constant(0.0,shape=[256],dtype=tf.float32),trainable=trainable,name='biases')
-              conv = tf.nn.conv2d(self.pool4,kernel,[1,1,1,1],padding='SAME')
-              out = tf.nn.bias_add(conv,biases)
-              self.conv5 = self.leaky_activation(out)
-              self.weights +=[kernel]
-              self.biases +=[biases]
-          
-          self.pool5 = tf.nn.max_pool(self.conv5,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool5')
+      with tf.name_scope('conv6') as scope:
+        conv6 = self._leakyConv(pool5, [3,3,256,512])
+      pool6 = tf.nn.max_pool(conv6,ksize=[1,2,2,1],strides=[1,1,1,1],padding='SAME',name='pool6')
   
-          with tf.name_scope('conv6') as scope:
-              kernel=tf.Variable(tf.truncated_normal([3,3,256,512],dtype=tf.float32,stddev=1e-1),name='weights',trainable=trainable)
-              biases = tf.Variable(tf.constant(0.0,shape=[512],dtype=tf.float32),trainable=trainable,name='biases')
-              conv = tf.nn.conv2d(self.pool5,kernel,[1,1,1,1],padding='SAME')
-              out = tf.nn.bias_add(conv,biases)
-              self.conv6 = self.leaky_activation(out)
-              self.weights +=[kernel]
-              self.biases +=[biases]
-          
-          self.pool6 = tf.nn.max_pool(self.conv6,ksize=[1,2,2,1],strides=[1,1,1,1],padding='SAME',name='pool6')
+      with tf.name_scope('conv7') as scope:
+        conv7 = self._leakyConv(pool6, [3,3,512,1024])
   
-          with tf.name_scope('conv7') as scope:
-              kernel=tf.Variable(tf.truncated_normal([3,3,512,1024],dtype=tf.float32,stddev=1e-1),name='weights',trainable=trainable)
-              biases = tf.Variable(tf.constant(0.0,shape=[1024],dtype=tf.float32),trainable=trainable,name='biases')
-              conv = tf.nn.conv2d(self.pool6,kernel,[1,1,1,1],padding='SAME')
-              out = tf.nn.bias_add(conv,biases)
-              self.conv7 = self.leaky_activation(out)
-              self.weights +=[kernel]
-              self.biases +=[biases]
+      with tf.name_scope('conv8') as scope:
+        conv8 = self._leakyConv(conv7, [3,3,1024,1024])
           
+      with tf.name_scope('conv9') as scope:
+        conv9 = self._linearConv(conv8, [1,1,1024,125])
+          
+      return conv9
+
+  def yoloV2ConvPipe(self, input_images):
+    with tf.name_scope('yolov2') as scope:
+      with tf.name_scope('conv1') as scope:
+        conv1 = self._leakyConv(input_images, [3,3,3,32])
+      pool1 = tf.nn.max_pool(conv1,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool1')
   
-          with tf.name_scope('conv8') as scope:
-              kernel=tf.Variable(tf.truncated_normal([3,3,1024,1024],dtype=tf.float32,stddev=1e-1),name='weights',trainable=trainable)
-              biases = tf.Variable(tf.constant(0.0,shape=[1024],dtype=tf.float32),trainable=trainable,name='biases')
-              conv = tf.nn.conv2d(self.conv7,kernel,[1,1,1,1],padding='SAME')
-              out = tf.nn.bias_add(conv,biases)
-              self.conv8 = self.leaky_activation(out)
-              self.weights +=[kernel]
-              self.biases +=[biases]
-          
-          with tf.name_scope('conv9') as scope:
-              kernel=tf.Variable(tf.truncated_normal([1,1,1024,125],dtype=tf.float32,stddev=1e-1),name='weights',trainable=trainable)
-              biases = tf.Variable(tf.constant(0.0,shape=[125],dtype=tf.float32),trainable=trainable,name='biases')
-              conv = tf.nn.conv2d(self.conv8,kernel,[1,1,1,1],padding='VALID')
-              self.conv9 = tf.nn.bias_add(conv,biases)
-              box_preds = self.conv9
-              self.weights +=[kernel]
-              self.biases +=[biases]
-          
-        return box_preds
+      with tf.name_scope('conv2') as scope:
+        conv2 = self._leakyConv(pool1, [3,3,32,64])
+      pool2 = tf.nn.max_pool(conv2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool2')
+ 
+      with tf.name_scope('conv3') as scope:
+        conv3 = self._leakyConv(pool2, [3,3,64,128])
+      with tf.name_scope('conv4') as scope:
+        conv4 = self._leakyConv(conv3, [1,1,128,64])
+      with tf.name_scope('conv5') as scope:
+        conv5 = self._leakyConv(conv4, [3,3,64,128])
+      pool3 = tf.nn.max_pool(conv5,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool2')
+ 
+      with tf.name_scope('conv6') as scope:
+        conv6 = self._leakyConv(pool3, [3,3,128,256])
+      with tf.name_scope('conv7') as scope:
+        conv7 = self._leakyConv(conv6, [1,1,256,128])
+      with tf.name_scope('conv8') as scope:
+        conv8 = self._leakyConv(conv7, [3,3,128,256])
+      pool4 = tf.nn.max_pool(conv8,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool2')
+ 
+      with tf.name_scope('conv9') as scope:
+        conv9 = self._leakyConv(pool4, [3,3,256,512])
+      with tf.name_scope('conv10') as scope:
+        conv10 = self._leakyConv(conv9, [1,1,512,256])
+      with tf.name_scope('conv11') as scope:
+        conv11 = self._leakyConv(conv10, [3,3,256,512])
+      with tf.name_scope('conv12') as scope:
+        conv12 = self._leakyConv(conv11, [1,1,512,256])
+      with tf.name_scope('conv13') as scope:
+        conv13 = self._leakyConv(conv12, [3,3,256,512])
+      pool5 = tf.nn.max_pool(conv13,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID',name='pool2')
 
+      with tf.name_scope('conv14') as scope:
+        conv14 = self._leakyConv(pool5, [3,3,512,1024])
+      with tf.name_scope('conv15') as scope:
+        conv15 = self._leakyConv(conv14, [1,1,1024,512])
+      with tf.name_scope('conv16') as scope:
+        conv16 = self._leakyConv(conv15, [3,3,512,1024])
+      with tf.name_scope('conv17') as scope:
+        conv17 = self._leakyConv(conv16, [1,1,1024,512])
+      with tf.name_scope('conv18') as scope:
+        conv18 = self._leakyConv(conv17, [3,3,512,1024])
+ 
+      with tf.name_scope('conv19') as scope:
+        conv19 = self._leakyConv(conv18, [3,3,1024,1024])
+      with tf.name_scope('conv20') as scope:
+        conv20 = self._leakyConv(conv19, [3,3,1024,1024])
 
-    def get_boxes(self,box_preds):
+      # routes
+      with tf.name_scope('conv21') as scope:
+        conv21 = self._leakyConv(conv13, [1,1,512,64])
+      #reorg=tf.concat([conv21[:,::2,::2,:],conv21[:,::2,1::2,:],conv21[:,1::2,::2,:],conv21[:,1::2,1::2,:]], axis=3)
+      reorg=tf.reshape(conv21, [-1, 13, 2, 13, 2, 64])
+      reorg=tf.transpose(reorg, [0, 1, 3, 2, 4, 5])
+      reorg=tf.reshape(reorg, [-1, 13, 13, 256])
+      route=tf.concat([reorg, conv20], axis=3)
 
-        global anchors
-        global cl_name
+      with tf.name_scope('conv22') as scope:
+        conv22 = self._leakyConv(route, [3,3,1280,1024])
 
-        bx_preds= tf.reshape(box_preds,[-1,13,13,5,25])
-        self.bx_preds = bx_preds
-        confs = tf.sigmoid(bx_preds[:,:,:,:,4])
-        class_probs =tf.nn.softmax(bx_preds[:,:,:,:,5:25])
-        max_class =tf.reduce_max(class_probs,axis=4)*confs
-        max_idx =tf.argmax(class_probs,axis=4)
+      with tf.name_scope('conv23') as scope:
+        conv23 = self._linearConv(conv22, [1,1,1024,125])
+      return conv23
 
-        indices = tf.where(max_class>0.20)
-        class_names =tf.gather(cl_name,tf.gather_nd(max_idx,indices))
-        scores = tf.gather_nd(max_class,indices)
-        
-        tx = tf.gather_nd(bx_preds[:,:,:,:,0],indices)
-        ty = tf.gather_nd(bx_preds[:,:,:,:,1],indices)
-        tw = tf.gather_nd(bx_preds[:,:,:,:,2],indices)
-        th = tf.gather_nd(bx_preds[:,:,:,:,3],indices)
-        batch_addr = indices[:,0]
-        x = (tf.cast(indices[:,2],tf.float32) + tf.sigmoid(tx))*32.0
-        y = (tf.cast(indices[:,1],tf.float32) + tf.sigmoid(ty))*32.0
-        w = tf.exp(tw)*tf.gather(anchors,2*indices[:,3])*16.0
-        h = tf.exp(th)*tf.gather(anchors,2*indices[:,3]+1)*16.0
-        x1=x-w
-        x2=x+w
-        y1=y-h
-        y2=y+h
-        boxes = tf.stack([x1,y1,x2,y2],axis=1)
-        indices = tf.image.non_max_suppression(boxes,scores,10,iou_threshold=0.3)
-        preds={'batch_addr':batch_addr, 'boxes':boxes,'indices':indices,'class_names':class_names}
-        return preds
+  def NMSPipe(self,box_preds):
 
+    global anchors
+    global cl_name
 
+    bx_preds= tf.reshape(box_preds,[-1,13,13,5,25])
+    self.bx_preds = bx_preds
+    confs = tf.sigmoid(bx_preds[:,:,:,:,4])
+    class_probs =tf.nn.softmax(bx_preds[:,:,:,:,5:25])
+    max_class =tf.reduce_max(class_probs,axis=4)*confs
+    self.max_class=max_class
+    max_idx =tf.argmax(class_probs,axis=4)
 
-
-
-
-
-
+    indices = tf.where(max_class>0.20)
+    class_names =tf.gather(self.cl_names,tf.gather_nd(max_idx,indices))
+    scores = tf.gather_nd(max_class,indices)
+    
+    tx = tf.gather_nd(bx_preds[:,:,:,:,0],indices)
+    ty = tf.gather_nd(bx_preds[:,:,:,:,1],indices)
+    tw = tf.gather_nd(bx_preds[:,:,:,:,2],indices)
+    th = tf.gather_nd(bx_preds[:,:,:,:,3],indices)
+    batch_addr = indices[:,0]
+    x = (tf.cast(indices[:,2],tf.float32) + tf.sigmoid(tx))*32.0
+    y = (tf.cast(indices[:,1],tf.float32) + tf.sigmoid(ty))*32.0
+    w = tf.exp(tw)*tf.gather(self.anchors,2*indices[:,3])*16.0
+    h = tf.exp(th)*tf.gather(self.anchors,2*indices[:,3]+1)*16.0
+    x1=x-w
+    x2=x+w
+    y1=y-h
+    y2=y+h
+    boxes = tf.stack([x1,y1,x2,y2],axis=1)
+    indices = tf.image.non_max_suppression(boxes,scores,10,iou_threshold=0.3)
+    preds={'batch_addr':batch_addr, 'boxes':boxes,'indices':indices,'class_names':class_names}
+    return preds
